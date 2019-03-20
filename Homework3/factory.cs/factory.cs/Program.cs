dc44824cf64f4834718328dcc9d522d47b12d172
StupidBirdAllerx <349443191@qq.com>
2019-03-20 23:35:15 +0800

    third

---
+++ Homework3/factory.cs/factory.cs/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factory.cs
+{
+    class Program
+    {
+        //日志记录器接口：抽象产品
+        interface Logger
+        {
+            void writeLog();
+        }
+        //数据库日志记录器：具体产品
+        class DatabaseLogger : Logger
+        {
+            public void writeLog()
+            {
+                Console.WriteLine("数据库日志记录。");
+            }
+        }
+        //文件日志记录器：具体产品
+        class FileLogger : Logger
+        {
+            public void writeLog()
+            {
+                Console.WriteLine("文件日志记录。");
+            }
+        }
+        //日志记录器工厂接口：抽象工厂
+        interface LoggerFactory
+        {
+            Logger createLogger();
+        }
+        //数据库日志记录器工厂类：具体工厂
+        class DatabaseLoggerFactory : LoggerFactory
+        {
+            public Logger createLogger()
+            {
+                //连接数据库，代码省略
+                //创建数据库日志记录器对象
+                Logger logger = new DatabaseLogger();
+                //初始化数据库日志记录器，代码省略
+                return logger;
+            }
+        }
+        //文件日志记录器工厂类：具体工厂
+        class FileLoggerFactory : LoggerFactory
+        {
+            public Logger createLogger()
+            {
+                //创建文件日志记录器对象
+                Logger logger = new FileLogger();
+                //创建文件，代码省略
+                return logger;
+            }
+        }
+        //测试代码
+        class Client
+        {
+            public static void Main(String [] args)
+            {
+                LoggerFactory factory;
+                Logger logger;
+                factory = new FileLoggerFactory();     //可引入配置文件实现
+                logger = factory.createLogger();
+                logger.writeLog();
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Homework3/simpleFactory/simpleFactory/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simpleFactory
+{
+    class Program
+    {
+        //抽象图表接口：抽象产品类
+        interface Chart
+        {
+            void display();
+        }
+        //柱状图类：具体产品类
+        class HistogramChart : Chart
+        {
+            public HistogramChart()
+            {
+                Console.WriteLine("创建柱状图！");
+            }
+            public void display()
+            {
+                Console.WriteLine("显示柱状图！");
+            }
+        }
+
+        //饼状图类：具体产品类
+        class PieChart : Chart
+        {
+            public PieChart()
+            {
+                Console.WriteLine("创建饼状图！");
+            }
+            public void display()
+            {
+                Console.WriteLine("显示饼状图！");
+            }
+        }
+        //折线图类：具体产品类
+        class LineChart : Chart
+        {
+            public LineChart()
+            {
+                Console.WriteLine("创建折线图！");
+            }
+            public void display()
+            {
+                Console.WriteLine("显示折线图！");
+            }
+        }
+        //图表工厂类：工厂类
+        class ChartFactory
+        {
+            //静态工厂方法
+            public static Chart getChart(String type)
+            {
+                Chart chart = null;
+                if (type == "histogram")
+                {
+                    chart = new HistogramChart();
+                    Console.WriteLine("初始化设置柱状图！");
+                }
+                else if (type == "pie")
+                {
+                    chart = new PieChart();
+                    Console.WriteLine("初始化设置饼状图！");
+                }
+                else if (type == "line")
+                {
+                    chart = new LineChart();
+                    Console.WriteLine("初始化设置折线图！");
+                }
+                return chart;
+            }
+        }
+        //测试代码
+        class Client
+        {
+            public static void Main(String [] args)
+            {
+                Chart chart;
+                chart = ChartFactory.getChart("histogram");    //通过静态工厂方法创建产品
+                chart.display();
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
